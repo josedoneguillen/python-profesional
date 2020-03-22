c9976dd8e03bc5ec6e2461b402e99154a376b284
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 13:29:37 -0400

    Ejemplo de variables y constantes

---
+++ conceptos/variables.py
@@ -0,0 +1,11 @@
+#declaracion de constante
+CONSTANTE = "soy una constante"
+
+#declaracion de variables
+nombre_tutor = "Codi"
+
+#Imprimir el contenido de la variable
+print(nombre_tutor)
+
+#Imprimir el contenido de la constante
+print(CONSTANTE)
