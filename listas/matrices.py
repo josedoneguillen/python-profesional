325e8a49a178ee626cc3aa098ede169523be0c86
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 15:02:00 -0400

    Ejemplos de matrices

---
+++ listas/matrices.py
@@ -0,0 +1,12 @@
+# Declaracion de listas
+fila_uno = [10, 20]
+fila_dos = [30, 40]
+
+# Declaracion de matriz
+matriz = [fila_uno, fila_dos]
+
+# Identificando primer elemento
+primer_elemeto =  matriz[0][0]
+
+# Imprimir
+print(primer_elemeto)
