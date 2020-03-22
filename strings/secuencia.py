5e480771b8cd9c14f3de887fb36e110dcb1c8264
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 16:01:28 -0400

    Ejemplos de strings

---
+++ strings/secuencia.py
@@ -0,0 +1,20 @@
+# Declaracion de variable string
+curso = "Curso de Python 3"
+
+# Imprimir primer elemento del string
+print(curso[0])
+
+# Sub string de la siguiente manera string[desde : hasta : saltos]
+resultado = curso[1:16:2]
+
+# Imprimir resultado
+print(resultado)
+
+# Contar longitud
+longitud = len(curso)
+
+# Imprimir resultado
+print(longitud)
+
+
+
