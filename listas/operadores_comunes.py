8012c6904defaa0cf42650a9b6288dd663d40388
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 14:57:28 -0400

    Ejemplos de operadores comunes para listas

---
+++ listas/operadores_comunes.py
@@ -0,0 +1,26 @@
+# Declarar Lista
+lista = [8.17, 90, 1, 5, 44, 1.32]
+
+# Ordernar Lista
+lista.sort(reverse=True)
+
+# Imprimir lista
+print(lista)
+
+# Imprimir el numero mas pequeño de la lista
+print(min(lista))
+
+# Imprimir el numero mas grande de la lista
+print(max(lista))
+
+# Imprimir la longitud de la lista
+print(len(lista))
+
+# buscar elemento en la lista
+print(8.17 in lista)
+
+# buscar indice de elemento en la lista
+print(lista.index(8.17))
+
+# contar elemento en la lista
+print(lista.count(8.17))
