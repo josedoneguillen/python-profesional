a8ffe0c3f59c547e93c2cdb98b4f3f407d705522
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 14:45:40 -0400

    Creando carpetas de listas con ejemplos de listas y sublistas

---
+++ listas/indices.py
@@ -0,0 +1,11 @@
+# Declaracion de lista de cursos
+cursos = ["Python", "django", "Flask", "C", "C++", "C#", "Java", "PHP"]
+
+# Declaracion de sub lista de cursos lista[desde : hasta : saltos]
+sub = cursos[0:3]
+
+#imprimir el ultimo elemento de la lista
+print(cursos[-1])
+
+#imprimir sublista lista
+print(sub)
---
+++ listas/listas.py
@@ -0,0 +1,5 @@
+# Declaracion de listas
+lista = ["Jose", 27, 130.5, True]
+
+# Imprimir la segunda posicion de la lista
+print(lista[1])
