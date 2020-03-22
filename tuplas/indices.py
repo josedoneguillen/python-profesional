4261ad170086c515effa54077e275ab25a909923
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 15:12:40 -0400

    Ejemplos de indice de tuplas

---
+++ tuplas/indices.py
@@ -0,0 +1,11 @@
+# Declaracion de tupla
+tupla = (1,2,3,4,5,6,7,8,9,0)
+
+# Declaracion de sub tupla de esta manera tupla[desde : hasta : saltos]
+sub = tupla[:9:2]
+
+#imprimir el ultimo elemento de la tupla
+print(tupla[-1])
+
+#imprimir subtupla tupla
+print(sub)
