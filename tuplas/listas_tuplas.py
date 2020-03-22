00fb320a39211d49a84d5aab9352ca6cb4469079
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 15:54:54 -0400

    Ejemplos de convertir tuplas listas y otros tipos entre si

---
+++ tuplas/listas_tuplas.py
@@ -0,0 +1,27 @@
+# Declaracion de lista
+lista = ["Curso", "Python", "CodigoFacilito"]
+
+# Declaracion de tupla
+tupla = ("Introduccion", "Basico", "Listas", "Tuplas")
+
+# Declaracion de variable string
+mensaje = "Hola"
+
+# Cambio de tupla a lista
+nueva_lista = list(tupla)
+
+# Imprimir la nueva lista
+print(nueva_lista)
+
+# Cambio de lista a tupla
+nueva_tupla = tuple(lista)
+
+# Imprimir la nueva tupla
+print(nueva_tupla)
+
+
+# Cambio de string a lista
+nuevo_mensaje = list(mensaje)
+
+# Imprimir el nuevo mensaje
+print(nuevo_mensaje)
