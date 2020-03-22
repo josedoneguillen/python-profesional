132aaca590328026ab10f87a44c7672b90a40568
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 13:44:13 -0400

    Ejemplo de operadores

---
+++ conceptos/operadores.py
@@ -0,0 +1,31 @@
+# Declaracion de variables
+var_uno = 10
+var_dos = 18
+
+# Operadores relacionales
+mayor = var_uno > var_dos
+menor= var_uno < var_dos
+mayor_igual = var_uno >= var_dos
+menor_igual = var_uno <= var_dos
+igual = var_uno == var_dos
+diferente = var_uno != var_dos
+
+# Imprimir valores
+print(mayor)
+print(menor)
+print(mayor_igual)
+print(menor_igual)
+print(igual)
+print(diferente)
+
+# Operadores logicos
+resultado_and = True and True and diferente
+resultado_or = True or True or diferente
+resultado_not = not True
+igual = var_uno is var_dos
+
+# Imprimir resultado de la operacion logica
+print(resultado_and)
+print(resultado_or)
+print(resultado_not)
+print(igual)
