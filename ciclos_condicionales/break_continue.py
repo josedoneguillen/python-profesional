f2fec5da58889e135d67d0fc9acf0b48ef38935a
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-23 11:05:48 -0400

    Ejemplo de uso break y continue

---
+++ ciclos_condicionales/break_continue.py
@@ -0,0 +1,17 @@
+# Declaracion de variable string
+titulo = "Curso de Python 3"
+
+# Ciclo for usando break
+for caracter in titulo:
+  # Condicional if  
+  if caracter == "P":
+    break
+  print(caracter)
+
+
+  # Ciclo for usando continue
+for caracter in titulo:
+  # Condicional if  
+  if caracter == "P":
+    continue
+  print(caracter)
