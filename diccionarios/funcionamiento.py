b939d7974f2f887823e78997e204f97227a6d494
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 23:03:58 -0400

    Ejemplos de diccionarios su funcionamiento

---
+++ diccionarios/funcionamiento.py
@@ -0,0 +1,14 @@
+# Declarar variable tipo diccionario
+diccionario = {"edad": 27, "peso": 130.5}
+
+# Asignar una llave y valor al diccionario
+diccionario["nombre"] = "Jose"
+
+# Imprimir diccionario
+print(diccionario)
+
+# Declarar variable tipo diccionario
+diccionario = {"a": 1, "b": 2, "c": 3, "a": 4}
+
+# Imprimir diccionario
+print(diccionario)
