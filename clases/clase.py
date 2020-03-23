e920270521ce56a1018a5ab7e259ae45c188a011
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-23 16:18:37 -0400

    Ejemplo de clase utilizando init

--- clases/clase.py
+++ clases/clase.py
@@ -2,10 +2,28 @@
 class Usuario:
   # Pass representa una sentencia nula
   pass
+  
+  # Constructor
+  def __init__(self, username="", correo="", nombre="") :
+      self.username = username
+      self.correo= correo
+      self.nombre = nombre
+
+  # Metodos
+  def saluda(self):
+    print("Hola, soy {} un usuario".format(self.nombre))      
 
 # Declarar un objeto instanciando la clase
-codi = Usuario()
-facilito = Usuario()
+codi = Usuario("josedone", "jdone@email.com", "Jose")
+#codi.username = "Jose"
+#codi.correo = "jdone@email.com"
+#facilito = Usuario()
 
 # Ver de que tipo es el objeto
 print(type(codi))
+
+# Usar el metodo saluda
+codi.saluda()
+
+# Imprimir email
+print(codi.correo)
