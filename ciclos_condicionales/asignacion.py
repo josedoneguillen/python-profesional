0d36f7ba621f8be93b3e4671e741c30709d361b1
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-23 11:12:11 -0400

    Ejemplo uso de condicionales en la asignacion

---
+++ ciclos_condicionales/asignacion.py
@@ -0,0 +1,22 @@
+# Declaracion de variables
+calificacion = 10
+color = None
+
+# Condicional if
+if calificacion >= 7 :
+  color = "verde"
+else :
+  color = "rojo"
+
+# Imprimir resultado
+print(calificacion, color)
+
+
+# Reducido
+
+# Declaracion de variables
+calificacion = 5
+color = "verde" if calificacion >= 7 else "rojo"
+
+# Imprimir resultado
+print(calificacion, color)
