880d25291a8676ae9311705a444940474b72ee82
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 23:42:01 -0400

    Ejemplo de condicional while

---
+++ ciclos_condicionales/while.py
@@ -0,0 +1,10 @@
+# Declaracion de variables
+numero = 123456789
+contador = 0
+
+# Ciclo while
+while numero >= 1 :
+  contador += 1
+  numero /= 10
+else :
+  print("La cantidad de digitos del numero es " + str(contador))  
