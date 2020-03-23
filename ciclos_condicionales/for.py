f4167f201e02d4386bd694b6f09ef65b3d44b559
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 23:47:54 -0400

    Ejemplo de ciclo for

---
+++ ciclos_condicionales/for.py
@@ -0,0 +1,11 @@
+# Declaracion de variables
+numeros = [1,2,3,4,5,6,7,8,9,10]
+numeros_diccionario = {"a": 1, "b": 2}
+
+# Ciclo for
+for numero in numeros :
+  print(numero)
+
+  # Ciclo for
+for numero_diccionario in numeros_diccionario :
+  print(numero_diccionario)
