a0c8286b13b299e3765426119013733b7711b75b
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 23:37:37 -0400

    Ejemplo de condicionales

---
+++ ciclos_condicionales/condicionales.py
@@ -0,0 +1,22 @@
+# Declaracion de variable
+color_luz = "amarillo"
+
+# Condicional if / else / elif
+if color_luz == "verde" :
+  print("Puede continuar")
+elif color_luz == "amarillo" :
+  print("Alto parcial")
+elif color_luz == "rojo" :
+  print("Alto total")  
+else :
+  print("El color no existe")  
+
+
+# Declaracion de variable
+variable = True
+
+# Condicional if / else / elif
+if (variable):
+  print("El valor es verdadero")
+else :
+  print("El valor es false")
