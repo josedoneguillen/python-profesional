cf6a081cd6e7694d1e5db576564d7a08b4824d69
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-22 23:16:02 -0400

    Ejemplos de obtener llaves y valores de diccionarios

---
+++ diccionarios/llaves.py
@@ -0,0 +1,20 @@
+# Declarar variable tipo diccionario
+diccionario = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+
+# Almacenar llaves en la variable resultados
+resultado = diccionario.keys()
+
+# Imprimir resultados
+print(resultado)
+
+# Almacenar valores en la variable resultados
+resultado = diccionario.values()
+
+# Imprimir resultados
+print(resultado)
+
+# Almacenar items en la variable resultados
+resultado = diccionario.items()
+
+# Imprimir resultados
+print(resultado)
