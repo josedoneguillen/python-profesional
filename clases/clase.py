fbb439ef640d44008222d0d4162c01ebbeaf38bd
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-23 14:56:19 -0400

    Ejemplo de creacion de clase y objetos

---
+++ clases/clase.py
@@ -0,0 +1,11 @@
+# Declarar una clase
+class Usuario:
+  # Pass representa una sentencia nula
+  pass
+
+# Declarar un objeto instanciando la clase
+codi = Usuario()
+facilito = Usuario()
+
+# Ver de que tipo es el objeto
+print(type(codi))
